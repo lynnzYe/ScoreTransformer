b743d41238f03317dcf8543c8e9cc10388e37bea
Linzan <lye11@u.rochester.edu>
2026-04-25 00:46:09 -0400

    add missing import in init

--- score_transformer/__init__.py
+++ score_transformer/__init__.py
@@ -1,8 +1,7 @@
-"""
-Author: Lynn Ye
-Created on: 2026/4/25
-Brief: 
-"""
+from .metric.ScoreSimilarity import score_similarity
+from .metric.ScoreSimilarity_orig import scoreSimilarity as score_similarity_orig
+from .detokenizer.tokens_to_score import tokens_to_score
+from .tokenizer.score_to_tokens import MusicXML_to_tokens as score_to_tokens
 
 
 def main():
