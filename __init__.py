383490b62d445914b6cec34517d51dd4c429042e
Linzan <lye11@u.rochester.edu>
2026-04-25 00:44:45 -0400

    clean up

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .metric.ScoreSimilarity import score_similarity
-from .metric.ScoreSimilarity_orig import scoreSimilarity as score_similarity_orig
-from .detokenizer.tokens_to_score import tokens_to_score
-from .tokenizer.score_to_tokens import MusicXML_to_tokens as score_to_tokens
---
+++ score_transformer/__init__.py
@@ -0,0 +1,13 @@
+"""
+Author: Lynn Ye
+Created on: 2026/4/25
+Brief: 
+"""
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
